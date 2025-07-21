07ec547f7058c80ae936b48ae6a69a9465d78cf0
sr-bioin <thapapradeep@gmail.com>
2025-07-21 16:53:01 -0400

    Update seurat_scRNA_seq.R

--- Seurat/seurat_scRNA_seq.R
+++ Seurat/seurat_scRNA_seq.R
@@ -134,39 +134,6 @@ plot1_spgene <- FeaturePlot(HCO_dat, c("NEUROD2","NEUROD6"), ncol = 1)
 plot2_spgene <- VlnPlot(HCO_dat, features = c("NEUROD2","NEUROD6"), pt.size = 0)
 plot1_spgene + plot2_spgene # + plot_layout(widths = c(1, 2))
 
-# Data set integration
-# Voxhunt
-
-load_aba_data("data/voxhunt_data/", lazy = TRUE) # Voxhunt library(https://data.mendeley.com/datasets/g4xg38mwcn/1)
-
-# 300 most variable genes from the E13.5 mouse brain
-genes_use <- variable_genes('E13', 300)$gene
-
-# Calculate the similarity map of a seurat object to the E13.5 mouse brain
-vox_map <- voxel_map(HCO_dat, genes_use=genes_use)
-plot_vox_map(vox_map)
-
-new_ident <- setNames(c("Dorsal telen. NPC",
-                        "Midbrain-hindbrain boundary neuron",
-                        "Dorsal telen. neuron",
-                        "Dien. and midbrain excitatory neuron",
-                        "MGE-like neuron","G2M dorsal telen. NPC",
-                        "Dorsal telen. IP","Dien. and midbrain NPC",
-                        "Dien. and midbrain IP and excitatory early neuron",
-                        "G2M Dien. and midbrain NPC",
-                        "G2M dorsal telen. NPC",
-                        "Dien. and midbrain inhibitory neuron",
-                        "Dien. and midbrain IP and early inhibitory neuron",
-                        "Ventral telen. neuron",
-                        "Unknown 1",
-                        "Unknown 2",
-                        "Unknown 3"),
-                      levels(HCO_dat))
-
-HCO_dat <- RenameIdents(HCO_dat, new_ident)
-plot_newIdent <- DimPlot(HCO_dat, reduction = "umap", label = TRUE) + NoLegend()
-plot_newIdent
-
 # Pseudotemporal cell ordering and variable genes
 HCO_dat_dorsal <- subset(HCO_dat, subset = RNA_snn_res.1 %in% c(0, 2, 5, 6, 10))
 HCO_dat_dorsal <- FindVariableFeatures(HCO_dat_dorsal, nfeatures = 2000)
